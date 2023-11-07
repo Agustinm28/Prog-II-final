8ef748879fa6806238a5060ca0e79bedba5edbc8
Agustín Montaña <81200417+Agustinm28@users.noreply.github.com>
2023-11-07 19:02:30 -0300

    generator first commit

---
+++ src/main/java/com/progii/finalgen/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package com.progii.finalgen.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+}
---
+++ src/test/java/com/progii/finalgen/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.progii.finalgen;
+
+import com.progii.finalgen.ProgIiFinalApp;
+import com.progii.finalgen.config.AsyncSyncConfiguration;
+import com.progii.finalgen.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ProgIiFinalApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
