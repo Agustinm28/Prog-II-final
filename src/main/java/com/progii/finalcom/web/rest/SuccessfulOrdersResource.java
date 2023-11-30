6a86904d2be086d0fcd03c2613e87624941c0331
Taccetta <n.taccetta@alumno.um.edu.ar>
2023-11-30 05:01:45 -0300

    mas proxys

--- src/main/java/com/progii/finalcom/web/rest/SuccessfulOrdersResource.java
+++ src/main/java/com/progii/finalcom/web/rest/SuccessfulOrdersResource.java
@@ -22,9 +22,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 import tech.jhipster.web.util.HeaderUtil;
 import tech.jhipster.web.util.PaginationUtil;
 import tech.jhipster.web.util.ResponseUtil;
@@ -187,6 +192,137 @@ public class SuccessfulOrdersResource {
             return ResponseEntity.ok(clientes);
         } catch (Exception e) {
             throw new BadRequestAlertException("Exception ", e.getMessage(), "Error getting clientes");
+        }
+    }
+
+    @GetMapping("/clientes/buscar")
+    public ResponseEntity<?> buscarClientes(
+        @RequestParam(name = "nombre", required = false) String nombre,
+        @RequestParam(name = "empresa", required = false) String empresa,
+        @org.springdoc.api.annotations.ParameterObject Pageable pageable
+    ) {
+        log.info("REST request to search for clientes");
+
+        String endpoint = "http://192.168.194.254:8000/api/clientes/buscar";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        UriComponentsBuilder builder = UriComponentsBuilder
+            .fromUriString(endpoint)
+            .queryParamIfPresent("nombre", Optional.ofNullable(nombre))
+            .queryParamIfPresent("empresa", Optional.ofNullable(empresa));
+
+        try {
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<?> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, Object.class);
+
+            return ResponseEntity.ok(response.getBody());
+        } catch (HttpClientErrorException.Unauthorized e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Unauthorized", e);
+        } catch (Exception e) {
+            throw new BadRequestAlertException("Exception", e.getMessage(), "Error searching for clientes");
+        }
+    }
+
+    @GetMapping("/acciones/")
+    public ResponseEntity<Object> getAcciones(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
+        log.info("REST request to get a page of acciones");
+        String endpoint = "http://192.168.194.254:8000/api/" + "acciones/";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        try {
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+            ResponseEntity<Object> response = restTemplate.exchange(endpoint, HttpMethod.GET, entity, Object.class);
+
+            Object responseBody = response.getBody();
+
+            // Verifica el tipo de la respuesta y realiza la deserialización según sea necesario
+            if (responseBody instanceof List) {
+                // La respuesta es un array
+                List<Map<String, Object>> acciones = (List<Map<String, Object>>) responseBody;
+                return ResponseEntity.ok(acciones);
+            } else if (responseBody instanceof Map) {
+                // La respuesta es un objeto
+                Map<String, Object> accion = (Map<String, Object>) responseBody;
+                List<Map<String, Object>> acciones = Collections.singletonList(accion);
+                return ResponseEntity.ok(acciones);
+            } else {
+                // Maneja otros casos según sea necesario
+                throw new BadRequestAlertException(
+                    "Respuesta inesperada del servidor",
+                    "Formato de respuesta no válido",
+                    "Error getting acciones"
+                );
+            }
+        } catch (Exception e) {
+            throw new BadRequestAlertException("Exception", e.getMessage(), "Error getting acciones");
+        }
+    }
+
+    @GetMapping("/ordenes/ordenes")
+    public ResponseEntity<Object> getOrdenes(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
+        log.info("REST request to get a page of ordenes");
+        String endpoint = "http://192.168.194.254:8000/api/ordenes/ordenes";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        try {
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+            ResponseEntity<Object> response = restTemplate.exchange(endpoint, HttpMethod.GET, entity, Object.class);
+
+            Object responseData = response.getBody();
+
+            // Aquí puedes verificar el tipo de la respuesta y manejarlo según sea necesario
+            if (responseData instanceof List) {
+                // Si es una lista, devuélvela directamente
+                return ResponseEntity.ok(responseData);
+            } else {
+                // Si es un objeto, podrías convertirlo a una lista o manejarlo de otra manera
+                // Aquí puedes realizar la conversión o el procesamiento necesario
+                List<Object> dataList = new ArrayList<>();
+                dataList.add(responseData);
+                return ResponseEntity.ok(dataList);
+            }
+        } catch (Exception e) {
+            throw new BadRequestAlertException("Exception", e.getMessage(), "Error getting ordenes");
+        }
+    }
+
+    @GetMapping("/reporte-operaciones/consulta")
+    public ResponseEntity<Object> getReporte(@org.springdoc.api.annotations.ParameterObject Pageable pageable) {
+        log.info("REST request to get a page of ordenes");
+        String endpoint = "http://192.168.194.254:8000/api/reporte-operaciones/consulta";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + token);
+
+        try {
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+            ResponseEntity<Object> response = restTemplate.exchange(endpoint, HttpMethod.GET, entity, Object.class);
+
+            Object responseData = response.getBody();
+
+            // Aquí puedes verificar el tipo de la respuesta y manejarlo según sea necesario
+            if (responseData instanceof List) {
+                // Si es una lista, devuélvela directamente
+                return ResponseEntity.ok(responseData);
+            } else {
+                // Si es un objeto, podrías convertirlo a una lista o manejarlo de otra manera
+                // Aquí puedes realizar la conversión o el procesamiento necesario
+                List<Object> dataList = new ArrayList<>();
+                dataList.add(responseData);
+                return ResponseEntity.ok(dataList);
+            }
+        } catch (Exception e) {
+            throw new BadRequestAlertException("Exception", e.getMessage(), "Error getting Reporte");
         }
     }
 
